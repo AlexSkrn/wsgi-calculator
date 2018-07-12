7e12c30d2a7dac6a2733a90fd4611c8bf295f6ea
alexskrn <askrn123@gmail.com>
2018-07-13 00:59:40 +0300

    Added all math functions then refactored using decorators

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import traceback
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -41,32 +42,155 @@ To submit your homework:
 
 """
 
+def home():
+    """Provide an index page with user instructions."""
+    page = """
+    <h1>Here's how to use this calculator page...</h1>
+    <p>You perform basic math operations by visiting addresses like this:</p>
+      <p><a href="http://localhost:8080/multiply/3/5">http://localhost:8080/multiply/3/5</a>   => 15</p>
+      <p><a href="http://localhost:8080/add/23/42">http://localhost:8080/add/23/42</a>      => 65</p>
+      <p><a href="http://localhost:8080/subtract/23/42">http://localhost:8080/subtract/23/42</a> => -19</p>
+      <p><a href="http://localhost:8080/divide/22/11">http://localhost:8080/divide/22/11</a>   => 2</p>
+    """
 
+    return page
+
+
+def decorate_math(math_func):
+    """Provide a decortating func for math functions."""
+    def return_func(*args):
+        """Wrap up the math func output."""
+        template = """
+            <h1>{}</h1>
+            <p><br /></p>
+            <a href="/">Back to index</a>
+        """
+        try:
+            res = math_func(*args)
+        except ZeroDivisionError:
+            raise ZeroDivisionError
+        except ValueError:
+            raise ValueError
+        return template.format(res)
+
+    return return_func
+
+
+@decorate_math
 def add(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """Return a STRING with the sum of the arguments."""
+    return str(sum(map(float, args)))
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+# def add(*args):
+#     """Return a STRING with the sum of the arguments."""
+#     # TODO: Fill sum with the correct value, based on the
+#     # args provided.
+#     template = """
+#         <h1>{}</h1>
+#         <p><br /></p>
+#         <a href="/">Back to index</a>
+#     """
+#     try:
+#         total = template.format(str(sum(map(float, args))))
+#     except ValueError:
+#         raise ValueError
+#
+#     return total
 
-    return sum
 
-# TODO: Add functions for handling more arithmetic operations.
+@decorate_math
+def divide(*args):
+    """Return a STRING with the quotient of the arguments."""
+    return str(float(args[0])/float(args[1]))
+
+
+# def divide(*args):
+#     """Return a STRING with the quotient of the arguments."""
+#     template = """
+#         <h1>{}</h1>
+#         <p><br /></p>
+#         <a href="/">Back to index</a>
+#     """
+#     try:
+#         quotient = template.format(str(float(args[0])/float(args[1])))
+#     except ZeroDivisionError:
+#         raise ZeroDivisionError
+#     except ValueError:
+#         raise ValueError
+#
+#     return quotient
+
+
+@decorate_math
+def subtract(*args):
+    """Return a STRING with the diff of the arguments."""
+    return str(float(args[0]) - float(args[1]))
+
+
+# def subtract(*args):
+#     """Return a STRING with the diff of the arguments."""
+#     template = """
+#         <h1>{}</h1>
+#         <p><br /></p>
+#         <a href="/">Back to index</a>
+#     """
+#     try:
+#         difference = template.format(str(float(args[0]) - float(args[1])))
+#     except ValueError:
+#         raise ValueError
+#
+#     return difference
+
+
+@decorate_math
+def multiply(*args):
+    """Return a STRING with the product of the arguments."""
+    return str(float(args[0]) * float(args[1]))
+
+
+# def multiply(*args):
+#     """Return a STRING with the product of the arguments."""
+#     template = """
+#         <h1>{}</h1>
+#         <p><br /></p>
+#         <a href="/">Back to index</a>
+#     """
+#     try:
+#         product = template.format(str(float(args[0]) * float(args[1])))
+#     except ValueError:
+#         raise ValueError
+#
+#     return product
+
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
-
     # TODO: Provide correct values for func and args. The
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': home,
+        'add': add,
+        'divide': divide,
+        'subtract': subtract,
+        'multiply': multiply,
+    }
+
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
+
 
 def application(environ, start_response):
     # TODO: Your application code from the book database
@@ -76,9 +200,37 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    # pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except ZeroDivisionError:
+        status = "500 ZeroDivisionError"
+        body = "<h1>Can't devide by zero</h1>"
+        body += '<p><br /></p>'
+        body += '<a href="/">Back to index</a>'
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    # pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
